1ce71c73bd2c7f786d8f7d409dd04d810b4f5c4c
Juan Pablo Calao Perez <juan.calao@ix.co>
2019-10-09 17:37:27 -0500

    callbacks

--- __test__/callback.test.js
+++ __test__/callback.test.js
@@ -3,7 +3,7 @@ import { callbackHell } from '../callback'
 describe('Probando un callback', () => {
     test('callback', done => {
         function otherCallback(data) {
-            expect(data).toBe('Hola javascripters')
+            expect(data).toBe('Hola Javascripters')
             done()
         }
         callbackHell(otherCallback)
--- callback.js
+++ callback.js
@@ -1,3 +1,3 @@
 export function callbackHell(callback) {
-    callback('Hola javascripters')
+    callback('Hola Javascripters')
 }
