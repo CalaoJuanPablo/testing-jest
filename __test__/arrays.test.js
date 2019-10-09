37286f243f37d10424eab5ca1e92261949156bee
Juan Pablo Calao Perez <juan.calao@ix.co>
2019-10-09 15:48:37 -0500

    testing arrays

---
+++ __test__/arrays.test.js
@@ -0,0 +1,15 @@
+import { arrayFruits, arrayColors } from '../arrays'
+
+describe('Comprobar que existe o no un elemento', () => {
+    test('Contiene una banana', () => {
+        expect(arrayFruits()).toContain('banana')
+    })
+
+    test('No continene un platano', () => {
+        expect(arrayFruits()).not.toContain('platano')
+    })
+
+    test('Comprobar el tamaño de un arreglo', () => {
+        expect(arrayFruits()).toHaveLength(6)
+    })
+})
