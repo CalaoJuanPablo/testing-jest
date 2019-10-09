6b98a64cf18199ae5bf8a33251274f312a49fff3
Juan Pablo Calao Perez <juan.calao@ix.co>
2019-10-09 15:06:30 -0500

    float numbers

--- __test__/numbers.test.js
+++ __test__/numbers.test.js
@@ -16,4 +16,8 @@ describe('Comparacion de numeros', function(){
     test('Mayor igual que', function(){
         expect(numbers(2,2)).toBeLessThanOrEqual(4)
     })
+
+    test('Numeros flotantes', function(){
+        expect(numbers(0.2,0.2)).toBeCloseTo(0.4)
+    })
 })
