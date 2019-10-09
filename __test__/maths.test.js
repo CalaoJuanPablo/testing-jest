5238a0108486231acafdc6e464f1fb10030b8770
Juan Pablo Calao Perez <juan.calao@ix.co>
2019-10-09 11:45:43 -0500

    test maths

---
+++ __test__/maths.test.js
@@ -0,0 +1,19 @@
+import { sumar, restar, multiplicar, dividir } from '../maths.js'
+
+describe('Cálculos matemáticos', function() {
+    test('Prueba de sumas', function() {
+        expect(sumar(1,1)).toBe(2)
+    })
+
+    test('Prueba restar', function() {
+        expect(restar(1,1)).toBe(0)
+    })
+
+    test('Prueba multiplicar', function() {
+        expect(multiplicar(1,1)).toBe(1)
+    })
+
+    test('Prueba dividir', function() {
+        expect(dividir(1,1)).toBe(1)
+    })
+})
