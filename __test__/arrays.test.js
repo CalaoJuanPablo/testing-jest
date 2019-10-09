881c5a7fd7d1e3db1dd2baab3dbfb3ca06dc13a5
Juan Pablo Calao Perez <juan.calao@ix.co>
2019-10-09 16:11:46 -0500

    coverage 100%

--- __test__/arrays.test.js
+++ __test__/arrays.test.js
@@ -12,4 +12,8 @@ describe('Comprobar que existe o no un elemento', () => {
     test('Comprobar el tamaño de un arreglo', () => {
         expect(arrayFruits()).toHaveLength(6)
     })
+
+    test('Comprobar que existe un color', () => {
+        expect(arrayColors()).toContain('rojo')
+    })
 })
