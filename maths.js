0f93d1a1a22860a35afb928556f96b3d53d4e861
Juan Pablo Calao Perez <juan.calao@ix.co>
2019-10-09 11:26:42 -0500

    first test

--- maths.js
+++ maths.js
@@ -1,16 +1,16 @@
-function sumar(a, b) {
+export function sumar(a, b) {
     return a + b
 }
 
-function restar(a, b) {
+export function restar(a, b) {
     return a - b
 }
 
-function multiplicar(a, b) {
+export function multiplicar(a, b) {
     return a * b
 }
 
 
-function dividir(a, b) {
+export function dividir(a, b) {
     return a / b
 }
